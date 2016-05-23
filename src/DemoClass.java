9fa73ef9657483f3a5200f8e949abaec841ed39a
chandani.thacker <chandani.thacker@attuneww.com>
2016-05-23 17:05:35 +0530

    1

---
+++ src/DemoClass.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+
+public class DemoClass {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<String> l=Arrays.asList("aa","bb","cc");
+		l.forEach(s->System.out.println(s));	
+	}
+}
